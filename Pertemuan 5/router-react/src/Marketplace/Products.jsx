f6d9fa9018c6f839e9b22b3d110a78e3a5b603fe
pramudyapolinema <1941720054@student.polinema.ac.id>
2022-03-22 21:25:47 +0700

    Update Tugas

--- Pertemuan 5/router-react/src/Marketplace/Products.jsx
+++ Pertemuan 5/router-react/src/Marketplace/Products.jsx
@@ -1,35 +1,65 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Switch, Route, useHistory, Redirect, useLocation } from "react-router-dom";
 import './Products.css';
 
 class Products extends Component {
     render() {
         return (
             <div>
+                <Router>
                 <section className="section-products">
                     <div className="container">
                         <div className="row justify-content-center text-center">
-                            <div className="col-md-8 col-lg-6">
+                            <div className="col-md-8 col-lg-12">
                                 <div className="header">
                                     <h3>Featured Product</h3>
-                                    <h2>Popular Products</h2>
+                                    <h2><Link to="/popular">Popular Products</Link> || <Link to="/random">Random Products</Link></h2>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <Item number="product-1" name="Sepatu" price="250.000" image="https://source.unsplash.com/random/300x300/?shoe"/>
-                            <Item number="product-2" name="Sepatu" price="150.000" image="https://source.unsplash.com/random/300x300/?shoe"/>
-                            <Item number="product-3" name="Sepatu" price="350.000" image="https://source.unsplash.com/random/300x300/?shoe"/>
-                            <Item number="product-4" name="Sepatu" price="450.000" image="https://source.unsplash.com/random/300x300/?shoe"/>
-                        </div>
+                        <Switch>
+                            <Route exact path="/">
+                                <Popular/>
+                            </Route>
+                            <Route path="/popular">
+                                <Popular/>
+                            </Route>
+                            <Route path="/random">
+                                <Random/>
+                            </Route>
+                        </Switch>
                     </div>
                 </section>
-                <hr/>
+                <hr />
+                </Router>
             </div>
         )
     }
 }
 
 export default Products;
+
+function Popular(){
+    return(
+        <div className="row">
+            <Item number="product-1" name="Sepatu" price="250.000" image="https://source.unsplash.com/random/300x300/?shoe" />
+            <Item number="product-2" name="Baju" price="150.000" image="https://source.unsplash.com/random/300x300/?shirt" />
+            <Item number="product-3" name="Topi" price="350.000" image="https://source.unsplash.com/random/300x300/?hat" />
+            <Item number="product-4" name="Celana" price="450.000" image="https://source.unsplash.com/random/300x300/?trouser" />
+        </div>
+    )
+}
+
+function Random(){
+    return(
+        <div className="row">
+            <Item number="product-1" name="Handphone" price="2.500.000" image="https://source.unsplash.com/random/300x300/?handphone" />
+            <Item number="product-2" name="Laptop" price="15.000.000" image="https://source.unsplash.com/random/300x300/?laptop" />
+            <Item number="product-3" name="Topi" price="350.000" image="https://source.unsplash.com/random/300x300/?hat" />
+            <Item number="product-4" name="Celana" price="450.000" image="https://source.unsplash.com/random/300x300/?trouser" />
+        </div>
+    )
+}
 
 const Item = (props) => {
     return (
--- Pertemuan 5/router-react/src/Marketplace/Profiles.jsx
+++ Pertemuan 5/router-react/src/Marketplace/Profiles.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Switch, Route, useHistory, Redirect, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useHistory, Redirect, useLocation } from "react-router-dom";
 import './Profiles.css';
 
 export default function Profiles() {
