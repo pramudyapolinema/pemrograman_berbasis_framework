219f0f5b63d3895f38650c20201e68f3991de429
pramudyapolinema <1941720054@student.polinema.ac.id>
2022-03-28 14:00:07 +0700

    Praktikum Pertemuan 6

---
+++ Pertemuan 6/redux/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {
+  Provider
+} from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
+  f)(createStore)(MainReducer)
+ReactDOM.render(
+  <Provider store = {store}>
+    <App/>
+  </Provider>, 
+document.getElementById('root'));
+
+reportWebVitals();
