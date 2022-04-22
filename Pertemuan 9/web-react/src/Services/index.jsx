7be9382d67e2e3343dbc695bd4dad94ff0652fea
pramudyapolinema <1941720054@student.polinema.ac.id>
2022-04-22 19:08:56 +0700

    Pertemuan 9 Praktikum 4

--- Pertemuan 9/web-react/src/Services/index.jsx
+++ Pertemuan 9/web-react/src/Services/index.jsx
@@ -1,51 +1,14 @@
-const domainPath = 'http://localhost:3001';
-const GetAPI = (path) => {
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}`)
-            .then(response => response.json())
-            .then((result) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
+import GetAPI from "./Get";
+import PostAPI from "./Post";
+import DeleteAPI from "./Delete";
 
-const PostAPI = (path, data) => {
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}`, {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then((result) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
-
-const DeleteAPI = (path, data) => {
-    const promise = new Promise((resolve, reject) => {
-        fetch(`${domainPath}/${path}/${data}`, {method: 'DELETE'})
-            .then((result) => {
-                resolve(result);
-            }, (err) => {
-                reject(err);
-            })
-    })
-    return promise;
-}
-
-
+// Daftar API - GET
 const getNewsBlog = () => GetAPI('posts?_sort=id&_order=desc');
+
+// Daftar API - POST
 const postNewsBlog = (dataYgDiKirim) => PostAPI('posts', dataYgDiKirim);
+
+// Daftar API - DELETE
 const deleteNewsBlog = (dataYgDiHapus) => DeleteAPI('posts', dataYgDiHapus);
 
 const API = {
