e0b0f460bd5daca7c005d37908d5c08f956721be
pramudyapolinema <1941720054@student.polinema.ac.id>
2022-05-07 14:59:40 +0700

    Pertemuan 10 Praktikum

---
+++ Pertemuan 10/praktikum-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC9iaEZ7kWhow10HHycO1EH1rB7qSNwkEI",
+    authDomain: "kelas3d-20.firebaseapp.com",
+    projectId: "kelas3d-20",
+    storageBucket: "kelas3d-20.appspot.com",
+    messagingSenderId: "310245268246",
+    appId: "1:310245268246:web:2a90babadfd724c0ce8e37",
+    measurementId: "G-F5FK8LYNLS"
+};
